aeb3ef04934fefaf5806d1ab4add845dffea94c2
pramath <pramathga@gmail.com>
2021-05-25 17:22:11 +0530

    added comment

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const http = require('http');
-const server=http.createServer((req , res)=>{
-    console.log(req.url, req.method); 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>myhh</title><head>');
-    res.write('<body><h1>hel</h1></body>');
-    res.write('</html>');
-    res.end();
-
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>myhh</title><head>");
+  res.write("<body><h1>hel</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen(8180);
+// demooo
